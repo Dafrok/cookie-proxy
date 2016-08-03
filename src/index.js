051b3ef1964cb1ea6195964bb7380f4821fee242
Dafrok <o.o@mug.dog>
2016-08-03 18:53:11 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export default new Proxy({}, {
+  get(target, name) {
+    const result = ''
+    const cookieList = document.cookie.split('; ')
+    for (const item of cookieList) {
+      const data = item.split('=')
+      if (data[0] === name) {
+        return data[1]
+      }
+    }
+  },
+  set(obj, prop, value) {
+    document.cookie = `${prop}=${value}`
+  }
+})
